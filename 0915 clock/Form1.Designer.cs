ecc74d949488b2a8b1d6f0896a3cef1fbd0cee0e
Oleksandra <predictormagnolia@gmail.com>
2023-09-15 23:14:32 +0300

    Add project files.

---
+++ 0915 clock/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace _0915_clock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(10, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(203, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(240, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "12";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 238);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "9";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(240, 457);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 37);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "6";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Yu Gothic UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(456, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 37);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "3";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 568);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ 0915 clock/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _0915_clock
+{
+    public partial class Form1 : Form
+    {
+        Pen clock;
+        Pen secondarrow;
+        Pen minutearrow;
+        Pen hourarrow;
+        Bitmap bmp;
+        Bitmap bmp2;
+        Graphics g;
+        Graphics gr;
+        Matrix mtr;
+        float alpha;
+        float alpha2;
+        float alpha3;
+
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(pictureBox1.Image = bmp);
+            gr = Graphics.FromImage(pictureBox1.Image = bmp);
+            mtr = new Matrix();
+
+            float InitialArrowPlacement = -135;
+            clock = new Pen(Color.Black, 10);
+            secondarrow = new Pen(Color.Black, 2);
+            minutearrow = new Pen(Color.Black, 4);
+            hourarrow = new Pen(Color.Black, 6);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr = Graphics.FromImage(pictureBox1.Image = bmp);
+            gr.DrawEllipse(clock, 10, 10, 480, 480);
+            gr.DrawEllipse(clock, 250, 250, 2, 2);
+
+            pictureBox1.Image = bmp;
+            alpha = InitialArrowPlacement+DateTime.Now.Second*6;
+            mtr.Rotate(alpha);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(secondarrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+            alpha2 = InitialArrowPlacement + DateTime.Now.Minute * 6;
+            mtr.Rotate(alpha2);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(minutearrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+            alpha3 = InitialArrowPlacement + DateTime.Now.Hour * 30 ;
+            mtr.Rotate(alpha3);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(hourarrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+            timer1.Start();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            label1.Text = String.Format("{0:D2}:{1:D2}:{2:D2}", dt.Hour, dt.Minute, dt.Second);
+            gr.Clear(Color.White);
+
+            clock = new Pen(Color.Black, 10);
+            secondarrow = new Pen(Color.Black, 2);
+            minutearrow = new Pen(Color.Black, 4);
+            hourarrow = new Pen(Color.Black, 6);
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            bmp2 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            gr = Graphics.FromImage(pictureBox1.Image = bmp);
+            gr.DrawEllipse(clock, 10, 10, 480, 480);
+            gr.DrawEllipse(clock, 250, 250, 2, 2);
+
+
+
+            pictureBox1.Image = bmp;
+            alpha += 6;
+            mtr.Rotate(alpha);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(secondarrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+            if (DateTime.Now.Second == 0)
+            {
+                alpha2 += 6;
+            }
+            mtr.Rotate(alpha2);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(minutearrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+            if (DateTime.Now.Minute == 0)
+            {
+                alpha3 += 30;
+            }
+            mtr.Rotate(alpha3);
+            gr.Transform = mtr;
+            gr.TranslateTransform(pictureBox1.Width / 2, pictureBox1.Height / 2, MatrixOrder.Append);
+            gr.DrawLine(hourarrow, 0, 0, 100, 100);
+            mtr.Reset();
+
+
+        }
+    }
+}
